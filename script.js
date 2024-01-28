13c060633e9056eb90e9c0e0af2de660ae688810
Sanjay Rautela <140192191+sanjayrautela@users.noreply.github.com>
2024-01-28 12:45:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
+// script.js
+
 function insert_Row() {
-    //Write your code here
-  
-  
+    // Get the table reference
+    var table = document.getElementById("sampleTable");
+
+    // Insert a new row at the top of the table
+    var newRow = table.insertRow(0);
+
+    // Insert cells into the new row
+    var cell1 = newRow.insertCell(0);
+    var cell2 = newRow.insertCell(1);
+
+    // Set the content of the cells (you can customize this)
+    cell1.innerHTML = "New Row Cell 1";
+    cell2.innerHTML = "New Row Cell 2";
 }
